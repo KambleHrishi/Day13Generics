620fbeba94b44ec10c286e83e46f616d314e6a2a
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-19 21:14:22 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day13Generics
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+        }
+    }
+}
